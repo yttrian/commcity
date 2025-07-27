1d8611a3d7c91fff4b44464f0b473ac48ee70275
Ian Moore <privet.privet250@gmail.com>
2025-07-27 14:36:13 -0400

    Configure for Dokku deployment (#1)
    * Add Heroku buildpacks and Procfile
    
    * Use DATABASE_URL
    
    * Add release tasks
    
    * Explain deployment

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mariadb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return [
 
         'mariadb' => [
             'driver' => 'mariadb',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
